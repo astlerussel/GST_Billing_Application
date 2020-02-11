79e02cb90400082979b1a6b51a530ab5c2c0697d
hiteshks <hiteshk888@gmail.com>
2020-02-11 19:24:03 +0530

    new bill button

--- app/src/main/java/app/hks/billy/MainActivity.java
+++ app/src/main/java/app/hks/billy/MainActivity.java
@@ -4,15 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
+import java.sql.BatchUpdateException;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar mToolBar;
+    private Button newBillButton;
 
     private CardView countCardView;
 
@@ -26,10 +31,21 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("Billy");
 
         countCardView = (CardView) findViewById(R.id.count_cardview);
+        newBillButton = (Button) findViewById(R.id.create_new_bill_button);
 
 
 
 
+
+
+
+        newBillButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =  new Intent(MainActivity.this, BillingActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
