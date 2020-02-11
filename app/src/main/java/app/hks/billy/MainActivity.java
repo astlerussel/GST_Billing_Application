680bceada705951a8f8f33a8e8815ab41e98771e
hiteshks <hiteshk888@gmail.com>
2020-02-11 20:44:31 +0530

    multi dex error

--- app/src/main/java/app/hks/billy/MainActivity.java
+++ app/src/main/java/app/hks/billy/MainActivity.java
@@ -1,5 +1,6 @@
 package app.hks.billy;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.cardview.widget.CardView;
@@ -11,6 +12,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
 import java.sql.BatchUpdateException;
 import java.util.Random;
 
@@ -20,6 +28,9 @@ public class MainActivity extends AppCompatActivity {
     private Button newBillButton;
 
     private CardView countCardView;
+    private FirebaseFirestore firebaseFirestore;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,19 +41,18 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(mToolBar);
         getSupportActionBar().setTitle("Billy");
 
+        firebaseFirestore = FirebaseFirestore.getInstance();
+
+        mAuth = FirebaseAuth.getInstance();
+
         countCardView = (CardView) findViewById(R.id.count_cardview);
         newBillButton = (Button) findViewById(R.id.create_new_bill_button);
-
-
-
-
-
 
 
         newBillButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent =  new Intent(MainActivity.this, BillingActivity.class);
+                Intent intent = new Intent(MainActivity.this, BillingActivity.class);
                 startActivity(intent);
             }
         });
@@ -53,6 +63,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
 
+        if (FirebaseAuth.getInstance().getCurrentUser() == null) {
+            Intent intent = new Intent(MainActivity.this, MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+            startActivity(intent);
+        } else {
+            String uid = mAuth.getCurrentUser().getUid();
+            final DocumentReference documentReference = firebaseFirestore.collection("users").document(uid);
+            documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+                @Override
+                public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+
+                    if (documentSnapshot.exists()) {
+                        String username = documentSnapshot.getString("name");
+                        //String age1 = documentSnapshot.getString("age");
+                        //String gender2 = documentSnapshot.getString("Gender");
+                        //String dob2 = documentSnapshot.getString("Date Of Birth");
+                        //String nationality2 = documentSnapshot.getString("Nationality");
+                        //||age1=="" || gender2=="" || nationality2==""|| dob2==""
+
+                        if (username == "") {
+                            Intent intent1 = new Intent(MainActivity.this, DetailsActivity.class);
+                            intent1.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            startActivity(intent1);
+                        }
+                    } else {
+                        Intent intent1 = new Intent(MainActivity.this, DetailsActivity.class);
+                        intent1.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent1);
+                    }
+
+                }
+            });
+
+        }
     }
 }
 
