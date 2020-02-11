d52f6245c020295753ef0f3a4aef0b99306ebabe
hiteshks <hiteshk888@gmail.com>
2020-02-11 18:11:27 +0530

    card Layout added

--- app/src/main/java/app/hks/billy/MainActivity.java
+++ app/src/main/java/app/hks/billy/MainActivity.java
@@ -2,13 +2,19 @@ package app.hks.billy;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar mToolBar;
+
+    private CardView countCardView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,19 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(mToolBar);
         getSupportActionBar().setTitle("Billy");
 
+        countCardView = (CardView) findViewById(R.id.count_cardview);
+
+
+
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
 
     }
 }
+
+
